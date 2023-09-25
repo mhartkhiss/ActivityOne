2b2670cae3726517cd3b6b7c69c6f24ad40f5509
Makizz <Makizz@KISSUNA>
2023-09-25 22:02:20 +0800

    First git commit

---
+++ src/activityone/ActivityOne.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package activityone;
+
+/**
+ *
+ * @author Makizz
+ */
+public class ActivityOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World! :)");
+        System.out.println("This is my Git commit test");
+    }
+    
+}
